91a080fb22b057cb070ff48d342e3967a14901dd
Brian Bienemann <brian.bienemann@amcom.com.br>
2020-01-20 18:24:25 -0300

    add vagrant builder

---
+++ scripts/install-jenkins.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -eux
+
+wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
+
+sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+
+sudo apt update
+
+sudo apt install -y jenkins
+
+sudo systemctl start jenkins
+
+#Verify status jenkinhs service
+#sudo systemctl status jenkins
+sudo cat /var/lib/jenkins/secrets/initialAdminPassword
